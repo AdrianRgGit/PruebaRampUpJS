1ea6369cbca4d3446963b5666e6b99834f4e0d30
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-06-01 10:53:49 +0200

    no me lee cuando cambio el valor en la página pero sí en el documento

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,19 +1,20 @@
-let string = "Hola buenos días a todos y todas";
+let string = "HOLA A TODOS";
 let totalLetras = 0;
 
 // Con función
 
 function contarLetras(letra) {
+    let stringMinusculas = string.toLowerCase();
     for (let i = 0; i < string.length; i++) {
-        if (letra === string[i]) {
+        if (letra === stringMinusculas[i]) {
             totalLetras += 1;
         }
     }
     return totalLetras;
 }
 
-contarLetras("h")
-console.log(`La función me retorna ${totalLetras}`)
+contarLetras("a")
+// console.log(`La función me retorna ${totalLetras}`)
 
 /*
 Con bucle for sin función
--- ejercicio2.js
+++ ejercicio2.js
@@ -1,25 +1,12 @@
-let nUsuario = 25;
+let input = document.querySelector("input#num");
+let nUsuario = input.getAttribute("value");
+
+// console.log(nUsuario);
 
 for (let i = 0; i <= nUsuario; i++) {
   if (i % 2 != 0) {
-    // console.log(i);
+    console.log(i);
   }
 }
 
-// Cambiar para que inserte número
 
-
-
-// inputNombre.setAttribute("value", "este es el nuevo valor");
-
-/*
-const ciudades = document.querySelector("#ciudades");
-
-const arrayCiudades = ["Burgos", "Zamora", "Cadiz", "León", "Sevilla"];
-
-arrayCiudades.forEach(function (elemento) {
-  const elementoLista = document.createElement("li");
-  elementoLista.textContent = elemento;
-  ciudades.append(elementoLista);
-});
-*/
