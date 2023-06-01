631ecf634761dffa9aa72915ab0b6353f55103b3
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-06-01 11:08:32 +0200

    arreglado lo del +50

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,12 +1,12 @@
 let input = document.querySelector("input#num");
 let nUsuario = input.getAttribute("value");
-
+let nUsuario50 = parseInt(nUsuario) + 50;
 // console.log(nUsuario);
+// console.log(nUsuario50);
 
-for (let i = 0; i <= nUsuario; i++) {
+for (let i = 0; i <= nUsuario50; i++) {
+  nUsuario = input.getAttribute("value");
   if (i % 2 != 0) {
     console.log(i);
   }
 }
-
-
